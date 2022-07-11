009d2235f72ac6bfaf0c61b5f0ee1ef20d237d9c
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-11 19:23:32 +0900

    DOCS: 네비게이션바 반응형 구현

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,7 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
   return (
-    <Navbar  bg="light" variant="pills" sticky="top" collapseOnSelect expand="lg">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="pills" sticky="top" >
     <Container>
         <Navbar.Brand>
             <Link to="Home" spy={true} smooth={true} duration={500}>
@@ -19,35 +19,38 @@ const Navigation = () => {
                 {/* 원래는 로고가 들어가야 함 */}
             </Link>
         </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-        <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
 
-      <Nav className="justify-content-end">
-        <Nav.Link><Link to="words" spy={true} smooth={true} duration={500}>
-            주일말씀
-        </Link></Nav.Link>
-        <Nav.Link><Link to="sermon" spy={true} smooth={true} duration={500}>
-            설교목록
-        </Link></Nav.Link>
-        <Nav.Link><Link to="service" spy={true} smooth={true} duration={500}>
-            예배안내
-        </Link></Nav.Link>
-        <Nav.Link><Link to="offering" spy={true} smooth={true} duration={500}>
-            온라인헌금
-        </Link></Nav.Link>
-        <Nav.Link><Link to="way" spy={true} smooth={true} duration={500}>
-            오시는길
-        </Link></Nav.Link>
-        <Nav.Link><Link to="minister" spy={true} smooth={true} duration={500}>
-            교역자안내
-        </Link></Nav.Link>
-        <Nav.Link><Link to="contact" spy={true} smooth={true} duration={500}>
-            CONTACT
-        </Link></Nav.Link>
-      </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="justify-content-end">
+                <Nav.Link><Link to="words" spy={true} smooth={true} duration={500}>
+                    주일말씀
+                </Link></Nav.Link>
+                <Nav.Link><Link to="sermon" spy={true} smooth={true} duration={500}>
+                    설교목록
+                </Link></Nav.Link>
+                <Nav.Link><Link to="service" spy={true} smooth={true} duration={500}>
+                    예배안내
+                </Link></Nav.Link>
+                <Nav.Link><Link to="offering" spy={true} smooth={true} duration={500}>
+                    온라인헌금
+                </Link></Nav.Link>
+                <Nav.Link><Link to="way" spy={true} smooth={true} duration={500}>
+                    오시는길
+                </Link></Nav.Link>
+                <Nav.Link><Link to="minister" spy={true} smooth={true} duration={500}>
+                    교역자안내
+                </Link></Nav.Link>
+                <Nav.Link><Link to="contact" spy={true} smooth={true} duration={500}>
+                    CONTACT
+                </Link></Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+
     </Container>
-  </Navbar>
-  );
+    </Navbar>
+);
 }
 
 export default Navigation;
