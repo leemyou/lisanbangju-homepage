50458a78d0309c124c42968de67c26920f5c2200
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-13 03:44:02 +0900

    Feat: 온라인 헌금페이지 퍼블리싱완료
    바로가기 링크 넣어야함.

--- src/pages/Offering.js
+++ src/pages/Offering.js
@@ -1,15 +1,28 @@
 // 온라인헌금 페이지
 
 import React from 'react'
-// import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Offering = () => {
+    const goKakao = () => {
+        // 기능추가 해야함
+        alert('카카오톡 바로가기로 이동기능 추가 요함')
+    }
+
   return (
     <div className='main-body' id="offering">
         <h1 className='main-title'>온라인 헌금</h1>
+        <hr className='main-title-line'/>
+
+        <div>
+            <h2>00은행&nbsp;&nbsp; 0000-000-000000&nbsp;&nbsp; 예금주:일산방주교회</h2>
+            <p>기부금영수증 신청 및 기타 문의사항은 일산방주교회 카카오톡으로 문의 부탁드립니다.</p>
+        
+            <button className='btn-offer-kakao' onClick={goKakao}>카카오톡 바로가기</button>
+        </div>
+
     </div>
   )
 }
 
 export default Offering
-
