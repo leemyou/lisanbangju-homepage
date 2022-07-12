c296be5ee9f83a16101b158d65090252fd333272
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-12 23:52:04 +0900

    Feat: 홈케러셀 완료

--- src/components/Carousels.js
+++ src/components/Carousels.js
@@ -1,50 +1,38 @@
 import Carousel from 'react-bootstrap/Carousel';
 import './Carousel.css'
 
-function IndividualIntervalsExample() {
+function CarouselFadeExample() {
   return (
-    <Carousel fade className="carousel">
-      <Carousel.Item interval={10000} className="carouse-item">
-        <h1>은혜로다</h1>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel fade>
+
+        <Carousel.Item className="carousel">
+            <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
 
 
+        <Carousel.Item className="carousel">
+            <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        
+        
+        <Carousel.Item className="carousel">
 
-      <Carousel.Item interval={10000} className="carouse-item">
-        {/* <img
-            style={{width:'300px', height:'100%', backgroundColor:'red'}}
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        /> */}
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+            </Carousel.Caption>
+        </Carousel.Item>
 
-
-
-      <Carousel.Item className="carouse-item" interval={10000}>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
     </Carousel>
   );
 }
 
-export default IndividualIntervalsExample;
-
+export default CarouselFadeExample;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,16 @@
 // 메인화면
 import React from 'react'
-import styled from 'styled-components';
 import Carousels from '../components/Carousels'
 
 
 const Home = () => {
     return (
-    // <Container id="Home">
-    //     <h1>Home</h1>
-    // </Container>
     <div className='main-body' id='Home'>
-        <h1 className='main-title'></h1>
-        <Carousels />
+        <div className='main-title'></div>
+        <Carousels style={{padding: '100px'}}/>
     </div>
 
     )
 }
 
 export default Home
-
-export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #c8c8c8;
-`
