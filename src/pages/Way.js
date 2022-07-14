6629d457f0c3283e9997598cce4407c479f33a66
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-14 16:41:05 +0900

    Feat: 지도 기능 구현
    카카오API 사용

--- src/pages/Way.js
+++ src/pages/Way.js
@@ -8,8 +8,9 @@ const Way = () => {
     <div className='main-body' id='way'>
         <h1 className='main-title'>오시는 길</h1>
         <hr className='main-title-line'/>
-
         <Map/>
+        <LocTitle>경기도 고양시 일산서구 일중로 17</LocTitle>
+        <LocSubtitle>(일산동 524-16) 포오스프라자 3층 일산방주교회 </LocSubtitle>
     </div>
   )
 
@@ -21,4 +22,13 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     background-color: #111;
+`
+const LocTitle = styled.h4 `
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 20px;
+`
+const LocSubtitle = styled.p`
+    text-align: center;
+    font-size: 1.1rem;
 `
