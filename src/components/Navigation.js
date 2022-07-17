312168d7765ff19b3212a4a9fd4fed3ec6e9955d
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-17 21:28:20 +0900

    Style: 네비게이션 백그라운드 색 조정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,45 +6,43 @@ import './Navigation.css'
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed='top'>
-      <Container>
-        <Navbar.Brand>
-            <Link to="Home" spy={true} smooth={true}>
-                <button className='nav-logo'></button>
-            </Link>
-        </Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed='top' style={{width: '100vw',}}>
+        <Container>
+            <Navbar.Brand>
+                <Link to="Home" spy={true} smooth={true}>
+                    <button className='nav-logo'></button>
+                </Link>
+            </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-                <Link to="words" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">주일말씀</button>
-                </Link>
-                <Link to="sermon" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">설교목록</button>
-                </Link>
-                <Link to="service" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">예배안내</button>
-                </Link>
-                <Link to="offering" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">온라인헌금</button>
-                </Link>
-                <Link to="way" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">오시는길</button>
-                </Link>
-                <Link to="minister" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">교역자안내</button>
-                </Link>
-                <Link to="contact" spy={true} smooth={true} duration={300}>
-                    <button className="btn-nav">CONTACT</button>
-                </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
-            </Nav>
-        <Nav>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                    <Link to="words" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">주일말씀</button>
+                    </Link>
+                    <Link to="sermon" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">설교목록</button>
+                    </Link>
+                    <Link to="service" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">예배안내</button>
+                    </Link>
+                    <Link to="offering" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">온라인헌금</button>
+                    </Link>
+                    <Link to="way" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">오시는길</button>
+                    </Link>
+                    <Link to="minister" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">교역자안내</button>
+                    </Link>
+                    <Link to="contact" spy={true} smooth={true} duration={300}>
+                        <button className="btn-nav">CONTACT</button>
+                    </Link>
+
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
     </Navbar>
   );
 }
