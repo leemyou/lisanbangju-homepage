fff55a5847ad85fea1f15b8a9e9bff7a625ab36f
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-18 00:26:31 +0900

    Feat: 반응형 구현

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ import instaimg from '../img/instagram.png';
 const Contact = () => {
 
     return (
-        <div id="contact" className="main-body" >
+        <div id="contact" >
             <h1 className='main-title'>CONTACT</h1>
             <hr className='main-title-line' />
 
@@ -18,34 +18,43 @@ const Contact = () => {
                 <ul >
                     <li>
                         <img src={kakaoimg} className='contact-img' alt='kakao'/>
-                        <h3>Kakao</h3>
-                        <p>
-                            일산방주교회 카카오톡 채널입니다 <br/>
-                            플러스 친구를 추가하시면 다양한 교회 소식들을<br/>
-                            카카오톡을 통해 받아보실 수 있습니다
-                        </p>
-                        <a className='contact-link' href='http://pf.kakao.com/_MCSNb'>바로가기</a>
+                        <div className='contact-text'>
+                            <h3>Kakao</h3>
+                            <p>
+                                일산방주교회 카카오톡 채널입니다 <br/>
+                                플러스 친구를 추가하시면 다양한 교회 소식들을<br/>
+                                카카오톡을 통해 받아보실 수 있습니다
+                            </p>
+                            <a className='contact-link' href='http://pf.kakao.com/_MCSNb'>바로가기</a>
+                        </div>
+                        
                     </li>
 
                     <li>
                         <img src={youtubeimg} alt='youtube' className='contact-img'/>
-                        <h3>YouTube</h3>
-                        <p>
-                            일산방주교회 유튜브 채널입니다 <br/>
-                            구독을 통해 실시간 온라인 예배를 드리실 수 있고, <br/>
-                            이전 예배 영상을 통해 예배를 드리실  수 있습니다
-                        </p>
-                        <a className='contact-link' href='https://www.youtube.com/channel/UCe0eUxng8zRUILZgSWiDaDQ/featured'>바로가기</a>
+                        <div className='contact-text'>
+                            <h3>YouTube</h3>
+                            <p>
+                                일산방주교회 유튜브 채널입니다 <br/>
+                                구독을 통해 실시간 온라인 예배를 드리실 수 있고, <br/>
+                                이전 예배 영상을 통해 예배를 드리실  수 있습니다
+                            </p>
+                            <a className='contact-link' href='https://www.youtube.com/channel/UCe0eUxng8zRUILZgSWiDaDQ/featured'>바로가기</a>
+                        </div>
+                        
                     </li>
 
                     <li>
                         <img src={instaimg} alt='instagram' className='contact-img'/>
-                        <h3>Instagram</h3>
-                        <p>일산방주교회 인스타그램입니다 <br/>
-                            팔로우하시면 교회소식들을 보실 수 있고,<br/>
-                            소통하실 수 있습니다.
-                        </p>
-                        <a className='contact-link' href='https://www.instagram.com/ilsan_bangju/'>바로가기</a>
+                        <div className='contact-text'>
+                            <h3>Instagram</h3>
+                            <p>일산방주교회 인스타그램입니다 <br/>
+                                팔로우하시면 교회소식들을 보실 수 있고,<br/>
+                                소통하실 수 있습니다.
+                            </p>
+                            <a className='contact-link' href='https://www.instagram.com/ilsan_bangju/'>바로가기</a>
+                        </div>
+                        
                     </li>
 
                 </ul>
